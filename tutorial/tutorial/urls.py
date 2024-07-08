fbc0182c0714de9f06359c3397417c2717081dc4
tkuramot <mabdman512@gmail.com>
2024-07-08 14:15:28 +0900

    adding  login to browsable api

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -14,17 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import include, path
 from rest_framework import routers
 
 from quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include('snippets.urls')),
+    path("", include("snippets.urls")),
+]
+
+urlpatterns += [
+    path("api-auth/", include("rest_framework.urls")),
 ]
